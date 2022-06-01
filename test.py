144396cca7cff4fda538e6f05ab644d55e989572
kaiquefonseca <45497631+kaiquefonseca@users.noreply.github.com>
2022-06-01 15:44:49 -0300

    Criando test para o container

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from app import app
 import unittest
+import docker as docker_py
+import pytest
 
 class Test(unittest.TestCase):
     def setUp(self):
         # cria uma instância do unittest, precisa do nome "setUp"
-        self.app = app.test_client('0.0.0.0')
+        self.app = app.test_client()
 
         # envia uma requisicao GET para a URL
         self.result = self.app.get('/')
@@ -16,8 +18,22 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Hello Kaique Fonseca")
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello Kaique Fonseca") 
+
+    def generate_container(request):
+    """ Create a container for all tests within class to run inside of """
+
+    con = docker.containers.run(image="test_image", tty=True, detach=True)
+    request.cls.container = con
     
-    def test_url(self):
-        # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Hello Kaique Fonseca")    
+ 
+    def run_tests_in_container(request):
+        """
+        Not sure how to get the contents of request.function 
+        to execute inside of the docker container
+        """
+
+        output = request.cls.container.exec_run('0.0.0.0') 
+
+        exit_code = output[0]
+        assert exit_code == 0    
